1cdcd8e1cf7092ddf4ce41d0a2507d5dba166a99
Rachita Nayak <50329040+rachitanayak@users.noreply.github.com>
2025-05-11 11:08:43 +0530

    update image styling

--- src/components/DisplayPicture.tsx
+++ src/components/DisplayPicture.tsx
@@ -7,8 +7,8 @@ interface DisplayPictureProps {
 function DisplayPicture({ src, alt, fitToContainer }: DisplayPictureProps) {
   return (
     <div className="flex justify-center items-center">
-      <div className="border-2 border-blue-700 w-[50dvh] h-[60dvh] flex justify-center items-center">
-        <div className="flex justify-center items-center border border-blue-700  w-[45dvh] h-[55dvh] p-2">
+      <div className="border-2 border-blue-700 flex justify-center items-center">
+        <div className="flex justify-center items-center border border-blue-700  w-[25dvh] h-[25dvh] md:w-[35dvh] md:h-[45dvh] p-2">
           <img
             className={`object-cover md:${
               fitToContainer ? "w-full h-full" : ""
